0ce8e02ae00cba64fcb6d03f0a732639eef72905
Noble-Mushtak <noblemushtak.public@gmail.com>
2019-05-19 13:53:02 -0400

    Added copy folder script

---
+++ README.md
@@ -0,0 +1,6 @@
+# Google Drive Scripts
+The following scripts are automations of some Google Drive tasks which may take a long time to do using the Google Drive GUI interface. To use any of these scripts, first, clone this GitHub repository and [follow the first two steps of this Quickstart guide](https://developers.google.com/drive/api/v3/quickstart/python). Make sure you download `credentials.json` into the working directory where you cloned the GitHub repository. Then, just use `python3` to run the script.
+
+The below list explains the function of each script:
+
+ * `copy_folder.py` recursively copies all of the files within a folder
---
+++ copy_folder.py
@@ -0,0 +1,117 @@
+import sys
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ["https://www.googleapis.com/auth/drive"]
+# MIME type of Google Drive folder:
+folder_mime_type = "application/vnd.google-apps.folder"
+# Name of root folder
+root_folder_name = "My Drive"
+
+def main():
+    creds = None
+    # If token.pickle exists, then load the saved credentials from the pickle file:
+    if os.path.exists("token.pickle"):
+        with open("token.pickle", "rb") as token:
+            creds = pickle.load(token)
+            
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                "credentials.json", SCOPES)
+            creds = flow.run_local_server()
+        # Save the credentials in token.pickle for the next run
+        with open("token.pickle", "wb") as token:
+            pickle.dump(creds, token)
+            
+    # Initialize the Drive v3 API
+    service = build("drive", "v3", credentials=creds)
+    print("For the following prompts, the \""+root_folder_name+"\" folder is assumed to be the root file of your Google Drive.")
+    
+    # Get the folder name from the user:
+    orig_folder_name = input("Enter the name of the folder you want to copy: ")
+    orig_folder = get_folder(service, orig_folder_name)
+    if orig_folder == None:
+        return
+    
+    new_folder_name = input("Enter the name of the folder you want the copy to end up in: ")
+    new_folder = get_folder(service, new_folder_name)
+    if new_folder == None:
+        return
+    
+    copy_folder(service, orig_folder, new_folder)
+
+def get_folder(service, folder_name):
+    # If folder is root folder, then return root:
+    if folder_name == root_folder_name:
+        root = service.files().get(fileId="root").execute()
+        return root
+    
+    # Search for files with that name:
+    results = service.files().list(q="name='"+folder_name+"'", fields="files(id, name, mimeType)").execute()
+    items = results.get("files", [])
+    # Remove all non-folders from the list:
+    for i in range(len(items)-1, -1, -1):
+        folder = items[i]
+        if folder.get("mimeType") != folder_mime_type:
+            del items[i]
+    
+    # If there are no folders, return an error:
+    if len(items) == 0:
+        print("No folders with that name found")
+        return None
+    # If there is only one folder, return that folder:
+    if len(items) == 1:
+        return items[0]
+    # If there are multiple folders, ask the user to choose one:
+    print("Multiple folders with that name found:")
+    for i, folder in enumerate(items):
+        print(str(i+1)+". "+folder.get("id"))
+    while True:
+        # Ask the user to enter a number and return the folder corresponding to that number if possible.
+        folder_index = input("Select the number corresponding to the correct folder ID: ")
+        try:
+            folder_index = int(folder_index)
+            if folder_index > 0 and folder_index <= len(items):
+                return items[folder_index-1]
+        except ValueError:
+            pass
+        print("No folder ID corresponding to that number")
+        
+def copy_folder(service, folder, new_folder):
+    print("Started copying folder", folder.get("name"))
+    # Create a copy of the folder
+    metadata = {
+        "name": folder.get("name"),
+        "mimeType": folder_mime_type,
+        "parents": [new_folder.get("id")]
+    }
+    folder_copy = service.files().create(body=metadata, fields="id, name, mimeType").execute()
+    
+    # Get all of the files in this folder:
+    results = service.files().list(q="'"+folder.get("id")+"' in parents", fields="files(id, name, mimeType)").execute()
+    items = results.get("files", [])
+    # Copy every file in this folder:
+    for file in items:
+        # Make a recursive call if this file is a folder:
+        if file.get("mimeType") == folder_mime_type:
+            copy_folder(service, file, folder_copy)
+        # Otherwise, just make a normal copy by copying the file and putting it in the right folder:
+        else:
+            metadata = {
+                "name": file.get("name"),
+                "parents": [folder_copy.get("id")]
+            }
+            new_file = service.files().copy(body=metadata, fileId=file.get("id"), fields="id, name, mimeType").execute()
+            print("Copied file", file.get("name"))
+    print("Finished copying folder", folder.get("name"))
+
+if __name__ == "__main__":
+    main()
